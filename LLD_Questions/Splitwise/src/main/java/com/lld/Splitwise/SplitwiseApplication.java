7dca717ae395424e2c76e8937085f258502e629e
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2026-04-10 10:47:23 +0530

    splitwise

---
+++ LLD_Questions/Splitwise/src/main/java/com/lld/Splitwise/SplitwiseApplication.java
@@ -0,0 +1,13 @@
+package com.lld.Splitwise;
+
+
+public class SplitwiseApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Splitwise application started successfully!");
+
+
+	}
+
+}
